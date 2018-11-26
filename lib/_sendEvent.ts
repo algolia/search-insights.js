f8d270904679dc5f04b0612c550641b98c0963cb
Youcef Mammar <youcef.mammar@algolia.com>
2018-11-26 19:39:00 +0100

    feat(V2): introduce a bulkSendEvent helper
    The purpose is to
    - add ability to send multiple events in one single request
    - implement the new endpoint

--- lib/_sendEvent.ts
+++ lib/_sendEvent.ts
@@ -1,65 +1,72 @@
-import { InsightsSearchClickEvent } from './click';
-import { InsightsSearchConversionEvent } from './conversion';
+import { InsightsSearchClickEvent } from "./click";
+import { InsightsSearchConversionEvent } from "./conversion";
 
-declare var process : {
+declare var process: {
   env: {
-    NODE_ENV: string
-  }
-}
+    NODE_ENV: string;
+  };
+};
 
+export type InsightsEventTypes = "click" | "conversion";
 export type InsightsEvent = {
+  eventType: InsightsEventTypes;
+  eventName: string;
   userID: string;
   timestamp: number;
   indexName: string;
-}
-
-export type InsightsEventTypes = 'click'|'conversion'
+};
 
 /**
  *  Sends data to endpoint
- * @param eventType 'click'|'conversion'
- * @param eventData InsightsSearchConversionEvent|InsightsSearchClickEvent
+ * @param eventType InsightsEventTypes
+ * @param eventData InsightsSearchClickEvent|InsightsSearchConversionEvent
  */
-export function sendEvent(eventType: InsightsEventTypes, eventData: InsightsSearchClickEvent | InsightsSearchConversionEvent) {
-
+export function sendEvent(
+  eventType: InsightsEventTypes,
+  eventData: InsightsSearchClickEvent | InsightsSearchConversionEvent
+) {
   // Add client timestamp and userID
-  eventData.timestamp = Date.now()
+  eventData.timestamp = Date.now();
   eventData.userID = this._userID;
+  eventData.eventType = eventType;
+  // TODO: check eventType is matching eventData
+
+  bulkSendEvent(this._applicationID, this.apiKey, [eventData]);
+}
+
+export function bulkSendEvent(
+  applicationID: string,
+  apiKey: string,
+  events: InsightsEvent[]
+) {
 
   // Origin
-  const reportingQueryOrigin = process.env.NODE_ENV === 'production' ? `https://insights.algolia.io/1/searches/${eventType}` : `http://localhost:8080/1/${eventType}`;
-
+  const reportingQueryOrigin =
+    process.env.NODE_ENV === "production"
+      ? `https://insights.algolia.io/1/events`
+      : `http://localhost:8080/1/events`;
   // Auth query
-  const reportingURL = reportingQueryOrigin + `?X-Algolia-Application-Id=${this._applicationID}&X-Algolia-API-Key=${this._apiKey}`;
+  const reportingURL =
+    reportingQueryOrigin +
+    `?X-Algolia-Application-Id=${applicationID}&X-Algolia-API-Key=${apiKey}`;
 
   // Detect navigator support
-  const supportsNavigator = navigator && typeof navigator.sendBeacon === 'function';
+  const supportsNavigator =
+    navigator && typeof navigator.sendBeacon === "function";
+
+  const data = { events };
 
   // Always try sending data through sendbeacon
-  if(supportsNavigator){
-    navigator.sendBeacon(reportingURL, JSON.stringify(eventData));
-
+  if (supportsNavigator) {
+    navigator.sendBeacon(reportingURL, JSON.stringify(data));
   } else {
     // Default to a synchronous XHR
-    const report = new XMLHttpRequest()
+    const report = new XMLHttpRequest();
 
     // Open connection
-    report.open('POST', reportingURL)
+    report.open("POST", reportingURL);
 
     // Save queryID if event is search
-    if(eventType === 'search'){
-      report.onreadystatechange = () => {
-        if (report.readyState === 4) {
-          // If response has responseText
-          if(report.responseText){
-            // Parse JSON
-            const response = report && report.responseText ? JSON.parse(report.responseText) : {};
-            this._lastQueryID = response.queryID;
-          }
-        }
-      }
-    }
-
-    report.send(JSON.stringify(eventData));
+    report.send(JSON.stringify(data));
   }
 }
