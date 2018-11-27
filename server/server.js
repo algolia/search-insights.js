fe1271d79e455869093ce085b5ec6133aebb355b
Youcef Mammar <youcef.mammar@algolia.com>
2018-11-27 19:25:19 +0100

    chore(V2): adapt integration tests
    - removed all endpoints on server as we only have /1/events now
    - split the integration test into more atomic checks
    - prepared tests to support later more integration tests (namely conversion & view)

--- server/server.js
+++ server/server.js
@@ -1,48 +1,33 @@
-
-/* 
+/*
   Used for serving static build file to do async js library loading and testing,
   Can also respond to click, search and conversion events with random id's
 */
-const express = require('express')
-      testServer = express(),
-      bodyParser = require('body-parser'),
-      cors = require('cors'),
-      fs = require('fs'),
-      crypto = require('crypto'),
-      path = require('path');
+const express = require("express");
+const testServer = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
-const dataFile = './tests/data.txt'
+const dataFile = "./tests/data.txt";
 const timeout = 1000;
 
-testServer.use(cors())
-testServer.use(bodyParser.json())
-testServer.use(bodyParser.text())
-testServer.use(express.static(path.join(__dirname, './../dist')))
-testServer.use(express.static(path.join(__dirname, './../tests/production')))
+testServer.use(cors());
+testServer.use(bodyParser.json());
+testServer.use(bodyParser.text());
+testServer.use(express.static(path.join(__dirname, "./../dist")));
+testServer.use(express.static(path.join(__dirname, "./../tests/production")));
 
-testServer.post('/1/search', function (req, res) {
-  fs.appendFile(dataFile, req.body + "\n")
-  res.send({
-    queryID: 1234567890
-  })
-})
-
-testServer.post('/1/click', function (req, res) {
-  fs.appendFile(dataFile, req.body + "\n")
-  setTimeout(() => {
-    res.status(200).json({message: "ok"})
-  }, timeout)
-})
-
-testServer.post('/1/conversion', function (req, res) {
-  fs.appendFile(dataFile, req.body + "\n")
-  setTimeout(() => {
-    res.status(200).json({message: "ok"})
-  }, timeout)
-})
+testServer.post("/1/events", function(req, res) {
+  fs.appendFile(dataFile, req.body + "\n", () => {
+    setTimeout(() => {
+      res.status(200).json({ message: "ok" });
+    }, timeout);
+  });
+});
 
 if (require.main === module) {
   testServer.listen(8080);
 }
 
-module.exports = testServer
+module.exports = testServer;
