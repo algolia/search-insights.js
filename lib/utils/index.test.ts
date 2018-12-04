0bf1b7138837118642651b5d5ec1d4bea0055ff0
Youcef Mammar <youcef.mammar@algolia.com>
2018-12-04 17:39:14 +0100

    fix(tests): use jest test.each

--- lib/utils/index.test.ts
+++ lib/utils/index.test.ts
@@ -1,57 +1,49 @@
 import { isFunction, isNumber, isString, isUndefined } from "./index";
 
 describe("isUndefined", () => {
-  const tests = [
-    { input: 42, expected: false },
-    { input: "a string", expected: false },
-    { input: undefined, expected: true },
-    { input: () => null, expected: false }
-  ];
-  for (const { input, expected } of tests) {
-    it(`should return ${expected} when passed ${input}`, () => {
-      expect(isUndefined(input)).toEqual(expected);
-    });
-  }
+  test.each`
+    input         | expected
+    ${42}         | ${false}
+    ${"a string"} | ${false}
+    ${undefined}  | ${true}
+    ${() => null} | ${false}
+  `("should return $expected when passed $input", ({ input, expected }) => {
+    expect(isUndefined(input)).toEqual(expected);
+  });
 });
 
 describe("isNumber", () => {
-  const tests = [
-    { input: 42, expected: true },
-    { input: "a string", expected: false },
-    { input: undefined, expected: false },
-    { input: () => null, expected: false }
-  ];
-  for (const { input, expected } of tests) {
-    it(`should return ${expected} when passed ${input}`, () => {
-      expect(isNumber(input)).toEqual(expected);
-    });
-  }
+  test.each`
+    input         | expected
+    ${42}         | ${true}
+    ${"a string"} | ${false}
+    ${undefined}  | ${false}
+    ${() => null} | ${false}
+  `("should return $expected when passed $input", ({ input, expected }) => {
+    expect(isNumber(input)).toEqual(expected);
+  });
 });
 
 describe("isString", () => {
-  const tests = [
-    { input: 42, expected: false },
-    { input: "a string", expected: true },
-    { input: undefined, expected: false },
-    { input: () => null, expected: false }
-  ];
-  for (const { input, expected } of tests) {
-    it(`should return ${expected} when passed ${input}`, () => {
-      expect(isString(input)).toEqual(expected);
-    });
-  }
+  test.each`
+    input         | expected
+    ${42}         | ${false}
+    ${"a string"} | ${true}
+    ${undefined}  | ${false}
+    ${() => null} | ${false}
+  `("should return $expected when passed $input", ({ input, expected }) => {
+    expect(isString(input)).toEqual(expected);
+  });
 });
 
 describe("isFunction", () => {
-  const tests = [
-    { input: 42, expected: false },
-    { input: "a string", expected: false },
-    { input: undefined, expected: false },
-    { input: () => null, expected: true }
-  ];
-  for (const { input, expected } of tests) {
-    it(`should return ${expected} when passed ${input}`, () => {
-      expect(isFunction(input)).toEqual(expected);
-    });
-  }
+  test.each`
+    input         | expected
+    ${42}         | ${false}
+    ${"a string"} | ${false}
+    ${undefined}  | ${false}
+    ${() => null} | ${true}
+  `("should return $expected when passed $input", ({ input, expected }) => {
+    expect(isFunction(input)).toEqual(expected);
+  });
 });
