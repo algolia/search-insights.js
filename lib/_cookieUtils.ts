66a4a694e8ed9f82e3a29aa1d8062eba897e6f11
Youcef Mammar <tkrugg@users.noreply.github.com>
2018-12-14 18:30:46 +0100

    feat(cookieDuration): add cookieDuration support (#61)
    * feat(cookieDuration): add cookieDuration parameter to init
    
    * feat(cookieDuration): add basic tests for cookieUtils
    
    - remove cookie reuinitialization at each init
    - move createUUID outside cookie methods for easier testing
    - add tests for createUUID

--- lib/_cookieUtils.ts
+++ lib/_cookieUtils.ts
@@ -1,3 +1,4 @@
+import { createUUID } from "./utils/index";
 // Cookie key
 const COOKIE_KEY = "_ALGOLIA";
 
@@ -7,9 +8,9 @@ const COOKIE_KEY = "_ALGOLIA";
  * @param {[type]} cvalue [description]
  * @param {[type]} exdays [description]
  */
-const setCookie = (cname: string, cvalue: string, exdays: number) => {
+const setCookie = (cname: string, cvalue: string, cookieDuration: number) => {
   const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  d.setTime(d.getTime() + cookieDuration);
   const expires = `expires=${d.toUTCString()}`;
   document.cookie = `${cname}=${cvalue};${expires};path=/`;
 };
@@ -39,29 +40,19 @@ const getCookie = (cname: string): string => {
  * Return new UUID
  * @return {[string]} new UUID
  */
-const checkUserIdCookie = (userSpecifiedID?: string | number): string => {
+const checkUserIdCookie = (
+  userSpecifiedID?: string | number,
+  cookieDuration?: number
+): string => {
   const userToken = getCookie(COOKIE_KEY);
 
   if (!userToken || userToken === "") {
     const newUUID = createUUID();
-    setCookie(COOKIE_KEY, newUUID, 10);
+    setCookie(COOKIE_KEY, newUUID, cookieDuration);
     return newUUID;
   }
-  setCookie(COOKIE_KEY, userToken, 10);
-  return userToken;
-};
 
-/**
- * Create UUID according to
- * https://www.ietf.org/rfc/rfc4122.txt
- * @return {[string]} generated UUID
- */
-const createUUID = () => {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+  return userToken;
 };
 
 const userToken = checkUserIdCookie;
