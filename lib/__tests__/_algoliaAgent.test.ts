927e74e9b2dcdbb3ebe35fae7d30422987d8a33c
Youcef Mammar <tkrugg@users.noreply.github.com>
2019-04-10 16:50:05 +0200

    feat(UserAgent): introduce addAlgoliaAgent method (#105)
    * feat(UserAgent): introduce addAlgoliaAgent method
    
    This introduces a addAlgoliaAgent method that can be also found in the
    search client.
    
    ```ts
    aa('addAlgoliaAgent', 'a random string')
    ```
    
    * feat(UserAgent): add space after ;

---
+++ lib/__tests__/_algoliaAgent.test.ts
@@ -0,0 +1,40 @@
+import AlgoliaInsights from "../insights";
+
+jest.mock("../../package.json", () => ({ version: "1.0.1" }));
+
+describe("algoliaAgent", () => {
+  beforeEach(() => {
+    AlgoliaInsights.init({ apiKey: "test", appId: "test" });
+  });
+
+  it("should initialize the client with a default algoliaAgent string", () => {
+    expect(AlgoliaInsights._ua).toEqual(
+      "Algolia insights for JavaScript (1.0.1)"
+    );
+    expect(AlgoliaInsights._uaURIEncoded).toEqual(
+      "Algolia%20insights%20for%20JavaScript%20(1.0.1)"
+    );
+  });
+
+  it("should allow adding a string to algoliaAgent", () => {
+    AlgoliaInsights.addAlgoliaAgent("other string");
+    expect(AlgoliaInsights._ua).toEqual(
+      "Algolia insights for JavaScript (1.0.1); other string"
+    );
+    expect(AlgoliaInsights._uaURIEncoded).toEqual(
+      "Algolia%20insights%20for%20JavaScript%20(1.0.1)%3B%20other%20string"
+    );
+  });
+
+  it("should duplicate a string when added twice", () => {
+    AlgoliaInsights.addAlgoliaAgent("duplicated string");
+    AlgoliaInsights.addAlgoliaAgent("duplicated string");
+
+    expect(AlgoliaInsights._ua).toEqual(
+      "Algolia insights for JavaScript (1.0.1); duplicated string"
+    );
+    expect(AlgoliaInsights._uaURIEncoded).toEqual(
+      "Algolia%20insights%20for%20JavaScript%20(1.0.1)%3B%20duplicated%20string"
+    );
+  });
+});
