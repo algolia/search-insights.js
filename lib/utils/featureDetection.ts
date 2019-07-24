ac43419a4f99b0bcbce3af4ae08a1c14bcfcf2a9
Eunjae Lee <eunjae.lee@algolia.com>
2019-07-24 15:52:51 +0200

    chore: add util functions and tests

--- lib/utils/featureDetection.ts
+++ lib/utils/featureDetection.ts
@@ -1,6 +1,32 @@
+import { request as nodeHttpRequest } from "http";
+
 export const supportsCookies = () => {
   try {
     return Boolean(navigator.cookieEnabled);
+  } catch (e) {
+    return false;
+  }
+};
+
+export const supportsSendBeacon = () => {
+  try {
+    return Boolean(navigator.sendBeacon);
+  } catch (e) {
+    return false;
+  }
+};
+
+export const supportsXMLHttpRequest = () => {
+  try {
+    return Boolean(XMLHttpRequest);
+  } catch (e) {
+    return false;
+  }
+};
+
+export const supportsNodeHttpModule = () => {
+  try {
+    return Boolean(nodeHttpRequest);
   } catch (e) {
     return false;
   }
