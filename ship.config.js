afd50d8adb7a890739a13ab17813129af77b9a5e
Eunjae Lee <eunjae.lee@algolia.com>
2019-12-24 15:19:49 +0100

    chore: update the version in the snippet at release

--- ship.config.js
+++ ship.config.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = {
   mergeStrategy: {
     toSameBranch: ["maint"],
@@ -6,5 +9,13 @@ module.exports = {
     }
   },
   buildCommand: () => "yarn build && /bin/bash ./pre-deploy.sh",
-  pullRequestReviewer: "@algolia/instantsearch-for-websites"
+  pullRequestReviewer: ["@algolia/instantsearch-for-websites"],
+  versionUpdated: ({ version, dir }) => {
+    const readmePath = path.resolve(dir, "README.md");
+    let content = fs.readFileSync(readmePath).toString();
+    const regex = /cdn\.jsdelivr\.net\/npm\/search-insights@(\d+?\.\d+?\.\d+?)/;
+    const newUrl = `cdn.jsdelivr.net/npm/search-insights@${version}`;
+    content = content.replace(regex, newUrl);
+    fs.writeFileSync(readmePath, content);
+  }
 };
