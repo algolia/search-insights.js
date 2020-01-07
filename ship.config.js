79a3be0b469785019ca28b4ce2974a1bededfcab
Eunjae Lee <eunjae.lee@algolia.com>
2020-01-07 10:39:14 +0900

    chore: update version on README.md only when it's a stable release

--- ship.config.js
+++ ship.config.js
@@ -10,12 +10,18 @@ module.exports = {
   },
   buildCommand: () => "yarn build && /bin/bash ./pre-deploy.sh",
   pullRequestReviewer: ["@algolia/instantsearch-for-websites"],
-  versionUpdated: ({ version, dir }) => {
-    const readmePath = path.resolve(dir, "README.md");
-    let content = fs.readFileSync(readmePath).toString();
-    const regex = /cdn\.jsdelivr\.net\/npm\/search-insights@(\d+?\.\d+?\.\d+?)/;
-    const newUrl = `cdn.jsdelivr.net/npm/search-insights@${version}`;
-    content = content.replace(regex, newUrl);
-    fs.writeFileSync(readmePath, content);
+  versionUpdated: ({ version, releaseType, dir }) => {
+    if (
+      releaseType === "major" ||
+      releaseType === "minor" ||
+      releaseType === "patch"
+    ) {
+      const readmePath = path.resolve(dir, "README.md");
+      let content = fs.readFileSync(readmePath).toString();
+      const regex = /cdn\.jsdelivr\.net\/npm\/search-insights@(\d+?\.\d+?\.\d+?)/;
+      const newUrl = `cdn.jsdelivr.net/npm/search-insights@${version}`;
+      content = content.replace(regex, newUrl);
+      fs.writeFileSync(readmePath, content);
+    }
   }
 };
