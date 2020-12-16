2c7a403348d7b1254e37b28c43d77a1adc84a2db
eunjae-lee <karis612@gmail.com>
2020-12-16 17:13:14 +0100

    fix: use browser requester on browser with bundler

--- lib/node.ts
+++ lib/node.ts
@@ -1,10 +1,17 @@
 import AlgoliaAnalytics from "./insights";
 import { getFunctionalInterface } from "./_getFunctionalInterface";
+import { getRequesterForBrowser } from "./utils/getRequesterForBrowser";
 import { getRequesterForNode } from "./utils/getRequesterForNode";
 import { RequestFnType } from "./utils/request";
+
+export { getRequesterForBrowser, getRequesterForNode };
 
 export function createInsightsClient(requestFn: RequestFnType) {
   return getFunctionalInterface(new AlgoliaAnalytics({ requestFn }));
 }
 
-export default createInsightsClient(getRequesterForNode());
+export default createInsightsClient(
+  typeof window !== "undefined"
+    ? getRequesterForBrowser()
+    : getRequesterForNode()
+);
