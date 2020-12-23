df62ffd31f2efd7437d8da4e8f53b4efc796a08c
Eunjae Lee <eunjae.lee@algolia.com>
2020-12-23 16:29:39 +0100

    fix: use browser requester on browser with bundler (#238)
    * fix: use browser requester on browser with bundler
    
    * add another entry for 'browser'
    
    * fix bundlesize config
    
    * revert umd filename back

--- index-node.cjs.js
+++ index-node.cjs.js
@@ -1,4 +1,4 @@
-const aa = require("./dist/search-insights.cjs.min.js");
+const aa = require("./dist/search-insights-node.cjs.min.js");
 
 module.exports = aa.default;
 Object.keys(aa).forEach(key => {
--- lib/entry-node-cjs.ts
+++ lib/entry-node-cjs.ts
@@ -1,7 +1,10 @@
 import AlgoliaAnalytics from "./insights";
 import { getFunctionalInterface } from "./_getFunctionalInterface";
+import { getRequesterForBrowser } from "./utils/getRequesterForBrowser";
 import { getRequesterForNode } from "./utils/getRequesterForNode";
 import { RequestFnType } from "./utils/request";
+
+export { getRequesterForBrowser, getRequesterForNode };
 
 export function createInsightsClient(requestFn: RequestFnType) {
   return getFunctionalInterface(new AlgoliaAnalytics({ requestFn }));
