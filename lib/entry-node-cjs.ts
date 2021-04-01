e03e9d09449018889cbe4b953f03f304df47aaea
eunjae-lee <karis612@gmail.com>
2021-04-01 11:42:36 +0200

    test WIP

--- lib/entry-node-cjs.ts
+++ lib/entry-node-cjs.ts
@@ -4,7 +4,12 @@ import { getRequesterForBrowser } from "./utils/getRequesterForBrowser";
 import { getRequesterForNode } from "./utils/getRequesterForNode";
 import { RequestFnType } from "./utils/request";
 
-export { getRequesterForBrowser, getRequesterForNode };
+export {
+  AlgoliaAnalytics,
+  getFunctionalInterface,
+  getRequesterForBrowser,
+  getRequesterForNode
+};
 
 export function createInsightsClient(requestFn: RequestFnType) {
   return getFunctionalInterface(new AlgoliaAnalytics({ requestFn }));
