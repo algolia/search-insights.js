1730e9fb3f71b09124b94cda582a039b37735aee
Youcef Mammar <youcef.mammar@algolia.com>
2021-07-09 21:18:49 +0200

    fix: provide iife build to prevent conflicts with AMD

--- rollup.config.js
+++ rollup.config.js
@@ -54,5 +54,15 @@ export default [
     },
     external: ["http", "https"],
     plugins: createPlugins()
+  },
+  {
+    input: "lib/entry-umd.ts",
+    output: {
+      format: "iife",
+      name: MODULE_NAME,
+      file: `./dist/${LIBRARY_OUTPUT_NAME}.iife.min.js`
+    },
+    external: ["http", "https"],
+    plugins: createPlugins()
   }
 ];
