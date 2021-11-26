e9f3a91a64fcb8e8b848ed433f328a775fc372b3
Eunjae Lee <karis612@gmail.com>
2021-11-26 15:50:56 +0100

    patch transporter.read instead of transporter.requester.send

--- lib/_bindSearchClient.ts
+++ lib/_bindSearchClient.ts
@@ -1,55 +1,61 @@
 import { SearchClient } from "algoliasearch";
 
-// https://stackoverflow.com/questions/48011353/how-to-unwrap-the-type-of-a-promise
-type Awaited<T> = T extends PromiseLike<infer U>
-  ? { 0: Awaited<U>; 1: U }[U extends PromiseLike<any> ? 0 : 1]
-  : T;
-
 export function bindSearchClient(searchClient: SearchClient) {
-  const requester = searchClient.transporter.requester;
-  type Request = Parameters<typeof requester.send>[0];
-  type Response = Awaited<ReturnType<typeof requester.send>>;
+  const read = searchClient.transporter.read;
+  type Request = Parameters<typeof read>[0];
+  type RequestOptions = Parameters<typeof read>[1];
   const getUserToken = () => this._userToken;
   const setResponse = response => (this._lastResponse = response);
 
-  // @ts-expect-error patching the client intentionally
-  searchClient.transporter.requester = {
-    send(request: Request): Readonly<Promise<Response>> {
+  // @ts-expect-error we are patching a read-only function
+  searchClient.transporter.read = async (
+    request: Request,
+    requestOptions?: RequestOptions
+  ) => {
+    if (
+      request.path === "1/indexes/*/queries" ||
+      /^1\/indexes\/.*?\/query$/.test(request.path)
+    ) {
       let newRequest: Request;
       try {
-        const data = JSON.parse(request.data);
+        // request.data is always Record<string, string> when it's a search call
+        const data = request.data as Record<string, string>;
 
         if (Array.isArray(data.requests)) {
           // multi index
           newRequest = {
             ...request,
-            data: JSON.stringify({
+            data: {
               ...data,
               requests: data.requests.map(request => ({
                 ...request,
                 clickAnalytics: true,
                 userToken: getUserToken()
               }))
-            })
+            }
           };
         } else {
           newRequest = {
             ...request,
-            data: JSON.stringify({
+            data: {
               ...data,
               clickAnalytics: true,
               userToken: getUserToken()
-            })
+            }
           };
         }
       } catch (e) {
         newRequest = request;
       }
 
-      return requester.send(newRequest).then(result => {
-        setResponse(result);
-        return result;
-      });
+      return read
+        .call(searchClient.transporter, newRequest, requestOptions)
+        .then(result => {
+          setResponse(result);
+          return result;
+        });
+    } else {
+      return read.call(searchClient.transporter, request, requestOptions);
     }
   };
 }
