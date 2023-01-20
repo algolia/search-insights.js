dbae04fce8e0e912f7f6796cedd8067f755b2f4e
Benjamin Hinchley <7188324+benhinchley@users.noreply.github.com>
2023-01-20 15:43:35 +1100

    implement handling of anonmyous id
    DAU-200
    
    Co-authored-by: jkaho <jessica.hong@algolia.com>
    Co-authored-by: Beatrice <beatrice94@users.noreply.github.com>

---
+++ src/eventEmitter.ts
@@ -0,0 +1,34 @@
+export type EventEmitterCallback = (
+    eventName: string,
+    data: unknown
+  ) => void;
+
+export class EventEmitter {
+    private handlers: { [handlerName: string]: Array<EventEmitterCallback> } = {};
+
+    emit(type: string, data: any) {
+        const handlerFuncs = this.handlers[type];
+        if (!handlerFuncs || handlerFuncs.length === 0) {
+            return;
+        }
+
+        handlerFuncs.forEach((fn) => {
+            try {
+                fn(type, data);
+            } catch (error) {
+                console.error(error);
+            }
+        });
+    }
+
+    /**
+     * Register event callback function for type.
+     */
+    on(type: string, callback: EventEmitterCallback) {
+        if (!this.handlers[type]) {
+            this.handlers[type] = [];
+        }
+
+        this.handlers[type].push(callback);
+    }
+}
