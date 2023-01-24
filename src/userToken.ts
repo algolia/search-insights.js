095adc7c3170a274f6b7061369e257c744c5a57c
Jessica Hong <jessica.hong@algolia.com>
2023-01-24 11:42:34 +1100

    chore: add InsightsRegion type, extract createUUID into its own function
    Co-authored-by: Beatrice <beatrice94@users.noreply.github.com>

--- src/userToken.ts
+++ src/userToken.ts
@@ -1,5 +1,6 @@
 import Cookies from "js-cookie";
 import { tld } from "./tld";
+import { createUUID } from "./utils/uuid";
 
 class ExpiringCookieStore {
   constructor(private lease: number = 60) {}
@@ -126,10 +127,6 @@ export class UserToken {
    * https://www.ietf.org/rfc/rfc4122.txt
    */
   private uuid() {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-      const r = (Math.random() * 16) | 0;
-      const v = c === "x" ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    });
+    return createUUID();
   }
 }
