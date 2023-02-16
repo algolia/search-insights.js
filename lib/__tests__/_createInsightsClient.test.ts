957dc7eab68974a988aaa9a90c562ea344a1699c
Haroen Viaene <hello@haroen.me>
2023-02-16 10:58:33 +0100

    lint

--- lib/__tests__/_createInsightsClient.test.ts
+++ lib/__tests__/_createInsightsClient.test.ts
@@ -1,32 +1,32 @@
-import { createInsightsClient } from '../_createInsightsClient';
+import { createInsightsClient } from "../_createInsightsClient";
 
-describe('createInsightsClient', () => {
+describe("createInsightsClient", () => {
   beforeEach(() => {
     window.AlgoliaAnalyticsObject = undefined;
   });
 
-  it('should return a function', () => {
-    expect(typeof createInsightsClient(() => {})).toBe('function');
+  it("should return a function", () => {
+    expect(typeof createInsightsClient(() => {})).toBe("function");
   });
 
-  it('registers itself to window', () => {
+  it("registers itself to window", () => {
     const aa = createInsightsClient(() => {});
 
-    expect(typeof window.AlgoliaAnalyticsObject).toBe('string');
+    expect(typeof window.AlgoliaAnalyticsObject).toBe("string");
     expect(window[window.AlgoliaAnalyticsObject!]).toBe(aa);
   });
 
   it("doesn't register itself to window if key is already taken", () => {
-    window.AlgoliaAnalyticsObject = 'existingKey';
+    window.AlgoliaAnalyticsObject = "existingKey";
     const aa = createInsightsClient(() => {});
 
-    expect(window.AlgoliaAnalyticsObject).toBe('existingKey');
+    expect(window.AlgoliaAnalyticsObject).toBe("existingKey");
     expect(window[window.AlgoliaAnalyticsObject!]).not.toBe(aa);
   });
 
   it("doesn't register if no window", () => {
     delete (global as any).window;
 
-    expect(typeof createInsightsClient(() => {})).toBe('function');
+    expect(typeof createInsightsClient(() => {})).toBe("function");
   });
 });
