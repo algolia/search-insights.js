59c479cf1f70736fb96a2e271b6390777be45855
Haroen Viaene <hello@haroen.me>
2023-02-16 11:33:42 +0100

    feat(api): expose window.AlgoliaAnalyticsObject if not yet set (#391)
    This allows us to detect a search-insights instance, regardless of
    whether it's using umd or cjs entry points.
    
    FX-2264

---
+++ lib/_createInsightsClient.ts
@@ -0,0 +1,21 @@
+import AlgoliaAnalytics from './insights';
+import { getFunctionalInterface } from './_getFunctionalInterface';
+import { RequestFnType } from './utils/request';
+import { createUUID } from './utils/uuid';
+
+export function createInsightsClient(requestFn: RequestFnType) {
+  const aa = getFunctionalInterface(new AlgoliaAnalytics({ requestFn }));
+
+  if (typeof window === 'object') {
+    if (!window.AlgoliaAnalyticsObject) {
+      let pointer: string;
+      do {
+        pointer = createUUID();
+      } while (window[pointer] !== undefined);
+      window.AlgoliaAnalyticsObject = pointer;
+      window[window.AlgoliaAnalyticsObject] = aa;
+    }
+  }
+
+  return aa;
+}
--- lib/entry-browser-cjs.ts
+++ lib/entry-browser-cjs.ts
@@ -1,14 +1,8 @@
-import AlgoliaAnalytics from "./insights";
-import { getFunctionalInterface } from "./_getFunctionalInterface";
+import { createInsightsClient } from "./_createInsightsClient";
 import { getRequesterForBrowser } from "./utils/getRequesterForBrowser";
 import { getRequesterForNode } from "./utils/getRequesterForNode";
-import { RequestFnType } from "./utils/request";
 
 export { getRequesterForBrowser, getRequesterForNode };
 export * from "./types";
-
-export function createInsightsClient(requestFn: RequestFnType) {
-  return getFunctionalInterface(new AlgoliaAnalytics({ requestFn }));
-}
 
 export default createInsightsClient(getRequesterForBrowser());
