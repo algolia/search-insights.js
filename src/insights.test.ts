a29e7442279d91a49cecb1bb10604106d2b43ed8
Dhaya <154633+dhayab@users.noreply.github.com>
2023-02-28 14:54:27 +0100

    insights tests are easier to read

--- src/insights.test.ts
+++ src/insights.test.ts
@@ -1,8 +1,11 @@
+import type { FetchMock } from 'jest-fetch-mock';
+
 import { AlgoliaInsights } from './insights';
 import type { InsightsAdditionalEventParams } from './insightsAPIBeaconClient';
 
 describe('insights', () => {
-  describe('uses custom credentials', () => {
+  describe('applies custom credentials', () => {
+    let insights: AlgoliaInsights;
     const additionalParams: InsightsAdditionalEventParams = {
       headers: {
         'X-Algolia-Application-Id': 'overrideApp123',
@@ -10,17 +13,25 @@ describe('insights', () => {
       },
     };
 
-    function assert<
-      TFunction extends keyof Omit<AlgoliaInsights, 'initialized'>
-    >(
-      methodName: TFunction,
-      payload: Parameters<AlgoliaInsights[TFunction]>[0]
-    ) {
-      const insights = new AlgoliaInsights([['init', 'app123', 'key123']]);
+    beforeEach(() => {
+      (fetch as FetchMock).mockClear();
+      insights = new AlgoliaInsights([['init', 'app123', 'key123']]);
       insights.setUserToken('usertoken123');
+    });
 
-      // @ts-expect-error
-      insights[methodName](payload, additionalParams);
+    it('when passed to `sendEvents`', () => {
+      insights.sendEvents(
+        [
+          {
+            eventName: 'Hit Clicked',
+            eventType: 'click',
+            index: 'index1',
+            objectIDs: ['12345'],
+            positions: [1],
+          },
+        ],
+        additionalParams
+      );
 
       expect(fetch).toHaveBeenLastCalledWith(
         expect.any(String),
@@ -28,85 +39,153 @@ describe('insights', () => {
           headers: expect.objectContaining(additionalParams.headers),
         })
       );
-    }
-
-    it('when passed to `sendEvents`', () => {
-      assert<'sendEvents'>('sendEvents', [
-        {
-          eventName: 'Hit Clicked',
-          eventType: 'click',
-          index: 'index1',
-          objectIDs: ['12345'],
-          positions: [1],
-        },
-      ]);
     });
 
     it('when passed to `clickedObjectIDsAfterSearch`', () => {
-      assert<'clickedObjectIDsAfterSearch'>('clickedObjectIDsAfterSearch', {
-        eventName: 'Hit Clicked',
-        index: 'index1',
-        objectIDs: ['12345'],
-        positions: [1],
-        queryID: 'queryID1',
-      });
+      insights.clickedObjectIDsAfterSearch(
+        {
+          eventName: 'Hit Clicked',
+          index: 'index1',
+          objectIDs: ['12345'],
+          positions: [1],
+          queryID: 'queryID1',
+        },
+        additionalParams
+      );
+
+      expect(fetch).toHaveBeenLastCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          headers: expect.objectContaining(additionalParams.headers),
+        })
+      );
     });
 
     it('when passed to `clickedObjectIDs`', () => {
-      assert<'clickedObjectIDs'>('clickedObjectIDs', {
-        eventName: 'Hit Clicked',
-        index: 'index1',
-        objectIDs: ['12345'],
-      });
+      insights.clickedObjectIDs(
+        {
+          eventName: 'Hit Clicked',
+          index: 'index1',
+          objectIDs: ['12345'],
+        },
+        additionalParams
+      );
+
+      expect(fetch).toHaveBeenLastCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          headers: expect.objectContaining(additionalParams.headers),
+        })
+      );
     });
 
     it('when passed to `clickedFilters`', () => {
-      assert<'clickedFilters'>('clickedFilters', {
-        eventName: 'Filters Clicked',
-        index: 'index1',
-        filters: ['brand:Apple'],
-      });
+      insights.clickedFilters(
+        {
+          eventName: 'Filters Clicked',
+          index: 'index1',
+          filters: ['brand:Apple'],
+        },
+        additionalParams
+      );
+
+      expect(fetch).toHaveBeenLastCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          headers: expect.objectContaining(additionalParams.headers),
+        })
+      );
     });
 
     it('when passed to `convertedObjectIDsAfterSearch`', () => {
-      assert<'convertedObjectIDsAfterSearch'>('convertedObjectIDsAfterSearch', {
-        eventName: 'Hit Converted',
-        index: 'index1',
-        objectIDs: ['12345'],
-        queryID: 'queryID1',
-      });
+      insights.convertedObjectIDsAfterSearch(
+        {
+          eventName: 'Hit Converted',
+          index: 'index1',
+          objectIDs: ['12345'],
+          queryID: 'queryID1',
+        },
+        additionalParams
+      );
+
+      expect(fetch).toHaveBeenLastCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          headers: expect.objectContaining(additionalParams.headers),
+        })
+      );
     });
 
     it('when passed to `convertedObjectIDs`', () => {
-      assert<'convertedObjectIDs'>('convertedObjectIDs', {
-        eventName: 'Hit Converted',
-        index: 'index1',
-        objectIDs: ['12345'],
-      });
+      insights.convertedObjectIDs(
+        {
+          eventName: 'Hit Converted',
+          index: 'index1',
+          objectIDs: ['12345'],
+        },
+        additionalParams
+      );
+
+      expect(fetch).toHaveBeenLastCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          headers: expect.objectContaining(additionalParams.headers),
+        })
+      );
     });
 
     it('when passed to `convertedFilters`', () => {
-      assert<'convertedFilters'>('convertedFilters', {
-        eventName: 'Filters Converted',
-        index: 'index1',
-        filters: ['brand:Apple'],
-      });
+      insights.convertedFilters(
+        {
+          eventName: 'Filters Converted',
+          index: 'index1',
+          filters: ['brand:Apple'],
+        },
+        additionalParams
+      );
+
+      expect(fetch).toHaveBeenLastCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          headers: expect.objectContaining(additionalParams.headers),
+        })
+      );
     });
 
     it('when passed to `viewedObjectIDs`', () => {
-      assert<'viewedObjectIDs'>('viewedObjectIDs', {
-        eventName: 'Hits Viewed',
-        index: 'index1',
-        objectIDs: ['12345', '67890'],
-      });
+      insights.viewedObjectIDs(
+        {
+          eventName: 'Hits Viewed',
+          index: 'index1',
+          objectIDs: ['12345', '67890'],
+        },
+        additionalParams
+      );
+
+      expect(fetch).toHaveBeenLastCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          headers: expect.objectContaining(additionalParams.headers),
+        })
+      );
     });
 
     it('when passed to `viewedFilters`', () => {
-      assert<'viewedFilters'>('viewedFilters', {
-        eventName: 'Filters Viewed',
-        index: 'index1',
-        filters: ['brand:Apple'],
-      });
+      insights.viewedFilters(
+        {
+          eventName: 'Filters Viewed',
+          index: 'index1',
+          filters: ['brand:Apple'],
+        },
+        additionalParams
+      );
+
+      expect(fetch).toHaveBeenLastCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          headers: expect.objectContaining(additionalParams.headers),
+        })
+      );
     });
   });
 });
