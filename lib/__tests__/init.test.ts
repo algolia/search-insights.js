9eca28cb14f539d16c6b6de957bdba329550b889
Fabien Motte <fabien.motte@algolia.com>
2023-03-13 17:25:44 +0100

    test: merge tests

--- lib/__tests__/init.test.ts
+++ lib/__tests__/init.test.ts
@@ -275,15 +275,6 @@ describe("init", () => {
     expect(analyticsInstance._useCookie).toBe(true);
     expect(analyticsInstance._cookieDuration).toBe(100);
     expect(analyticsInstance._userToken).toBe("myUserToken");
-  });
-  it("should merge with previous credentials when `partial` is `true`", () => {
-    analyticsInstance.init({
-      appId: "appId1",
-      apiKey: "apiKey1"
-    });
-
-    expect(analyticsInstance._appId).toBe("appId1");
-    expect(analyticsInstance._apiKey).toBe("apiKey1");
 
     analyticsInstance.init({
       appId: "appId2",
@@ -291,7 +282,7 @@ describe("init", () => {
     });
 
     expect(analyticsInstance._appId).toBe("appId2");
-    expect(analyticsInstance._apiKey).toBe("apiKey1");
+    expect(analyticsInstance._apiKey).toBe("apiKey2");
   });
 
   describe("callback for userToken", () => {
