ec331fb929f5efe24101d5d4ef3039d6a257c8c5
Fabien Motte <fabien.motte@algolia.com>
2023-03-14 17:00:14 +0100

    chore: update prettier and fix linting (#416)

--- lib/_createInsightsClient.ts
+++ lib/_createInsightsClient.ts
@@ -1,13 +1,13 @@
-import AlgoliaAnalytics from './insights';
-import { getFunctionalInterface } from './_getFunctionalInterface';
-import { RequestFnType } from './utils/request';
-import { createUUID } from './utils/uuid';
-import { version } from '../package.json';
+import AlgoliaAnalytics from "./insights";
+import { getFunctionalInterface } from "./_getFunctionalInterface";
+import { RequestFnType } from "./utils/request";
+import { createUUID } from "./utils/uuid";
+import { version } from "../package.json";
 
 export function createInsightsClient(requestFn: RequestFnType) {
   const aa = getFunctionalInterface(new AlgoliaAnalytics({ requestFn }));
 
-  if (typeof window === 'object') {
+  if (typeof window === "object") {
     if (!window.AlgoliaAnalyticsObject) {
       let pointer: string;
       do {
--- lib/utils/uuid.ts
+++ lib/utils/uuid.ts
@@ -4,7 +4,7 @@
  * @return {[string]} generated UUID
  */
 export const createUUID = () => {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
     const r = (Math.random() * 16) | 0;
     const v = c === "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
