4402b90ee212d29d54d43c1f6511c9cfa13d1d70
Dhaya <154633+dhayab@users.noreply.github.com>
2023-03-20 12:11:58 +0100

    ci: fix pre-deploy script

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-file_output=$(grep -oh "localhost:8080" dist/search-insights.min.js)
-file_output_insights=$(grep -oh "https://insights.algolia.io" dist/search-insights.min.js)
+file_output=$(grep -oh "localhost:8080" dist/search-insights.min.js | head -n 1)
+file_output_insights=$(grep -oh "https://insights.algolia.io" dist/search-insights.min.js | head -n 1)
 
 # echo "$file_output", "$file_output_insights"
 
 if [[ "$file_output" == "localhost:8080" ]]; then
-  echo "search-insights.js localhost address, meaning you tried to publish a dev version of the script"
+  echo "[pre-deploy.sh] search-insights.js localhost address, meaning you tried to publish a dev version of the script"
   exit 1
 elif [[ "$file_output_insights" == "https://insights.algolia.io" ]]; then
-  echo "search-insights.js has proper reporting address"
+  echo "[pre-deploy.sh] search-insights.js has proper reporting address"
   exit 0
 else
-  printf "nothing found"
+  echo "[pre-deploy.sh] nothing found"
   exit 1
 fi
