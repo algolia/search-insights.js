f9ca81703e286be4a7dff551dbc09c8cad2e720c
Jonathan Ingram <jon.ingram@algolia.com>
2023-09-01 11:20:14 +1000

    ignore dist folder when testing

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFiles: [
-    "./jest.setup.js"
-  ],
-  transformIgnorePatterns: [
-    "<rootDir>/lib/polyfills/*.js"
-  ],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFiles: ["./jest.setup.js"],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  transformIgnorePatterns: ["<rootDir>/lib/polyfills/*.js"],
   watchPlugins: [
     "jest-watch-typeahead/filename",
     "jest-watch-typeahead/testname"
   ],
   globals: {
-    "__DEV__": true,
-    "__FLAVOR__": "node-cjs"
+    __DEV__: true,
+    __FLAVOR__: "node-cjs"
   }
 };
