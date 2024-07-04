b7e02fbed4a8dc399e25acba86be5d7943d5eadb
Jason Berry <jason.berry@algolia.com>
2024-07-04 17:06:00 +1000

    feat: store only the most recent 5000 mappings

--- lib/utils/__tests__/objectQueryTracker.test.ts
+++ lib/utils/__tests__/objectQueryTracker.test.ts
@@ -4,6 +4,13 @@ import {
   removeQueryForObjects
 } from "../objectQueryTracker";
 
+const wait = (ms: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+const stored = () =>
+  JSON.parse(localStorage.getItem("AlgoliaObjectQueryCache")!);
+
 describe("objectQueryTracker", () => {
   beforeEach(() => {
     localStorage.clear();
@@ -17,12 +24,8 @@ describe("objectQueryTracker", () => {
 
       storeQueryForObject(index, objectID, queryID);
 
-      const storedQuery = JSON.parse(
-        localStorage.getItem("AlgoliaObjectQueryCache_myIndex")!
-      );
-
-      expect(storedQuery).toEqual({
-        [objectID]: [queryID, expect.any(Number)]
+      expect(stored()).toEqual({
+        [`${index}_${objectID}`]: [queryID, expect.any(Number)]
       });
     });
 
@@ -36,14 +39,47 @@ describe("objectQueryTracker", () => {
       storeQueryForObject(index, objectID1, queryID1);
       storeQueryForObject(index, objectID2, queryID2);
 
-      const storedQuery = JSON.parse(
-        localStorage.getItem("AlgoliaObjectQueryCache_myIndex")!
-      );
-
-      expect(storedQuery).toEqual({
-        [objectID1]: [queryID1, expect.any(Number)],
-        [objectID2]: [queryID2, expect.any(Number)]
+      expect(stored()).toEqual({
+        [`${index}_${objectID1}`]: [queryID1, expect.any(Number)],
+        [`${index}_${objectID2}`]: [queryID2, expect.any(Number)]
       });
+    });
+
+    it("removes the 1000 oldest query when the limit of 5000 is reached", async () => {
+      const index = "myIndex";
+      const oldestObjectID = "123";
+      const newestObjectID = "456";
+      const oldestQueryID = "abc";
+      const newestQueryID = "def";
+
+      storeQueryForObject(index, oldestObjectID, oldestQueryID);
+
+      expect(getQueryForObject(index, oldestObjectID)).toEqual([
+        oldestQueryID,
+        expect.any(Number)
+      ]);
+
+      await wait(1);
+
+      // synthetically generate 5000 entries
+      const store = stored();
+      for (let i = 0; i < 5000; i++) {
+        store[`${index}_object${i}`] = [`query${i}`, Date.now()];
+      }
+      localStorage.setItem("AlgoliaObjectQueryCache", JSON.stringify(store));
+
+      expect(Object.keys(stored())).toHaveLength(5001);
+
+      await wait(1);
+
+      storeQueryForObject(index, newestObjectID, newestQueryID);
+
+      expect(Object.keys(stored())).toHaveLength(4001);
+      expect(getQueryForObject(index, newestObjectID)).toEqual([
+        newestQueryID,
+        expect.any(Number)
+      ]);
+      expect(getQueryForObject(index, oldestObjectID)).toEqual(undefined);
     });
   });
 
@@ -83,22 +119,14 @@ describe("objectQueryTracker", () => {
       storeQueryForObject(index, objectID1, queryID1);
       storeQueryForObject(index, objectID2, queryID2);
 
-      const storedQuery = JSON.parse(
-        localStorage.getItem("AlgoliaObjectQueryCache_myIndex")!
-      );
-
-      expect(storedQuery).toEqual({
-        [objectID1]: [queryID1, expect.any(Number)],
-        [objectID2]: [queryID2, expect.any(Number)]
+      expect(stored()).toEqual({
+        [`${index}_${objectID1}`]: [queryID1, expect.any(Number)],
+        [`${index}_${objectID2}`]: [queryID2, expect.any(Number)]
       });
 
       removeQueryForObjects(index, [objectID1, objectID2]);
 
-      const storedQueryAfterRemoval = JSON.parse(
-        localStorage.getItem("AlgoliaObjectQueryCache_myIndex")!
-      );
-
-      expect(storedQueryAfterRemoval).toEqual({});
+      expect(stored()).toEqual({});
     });
   });
 });
