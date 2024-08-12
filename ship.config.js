b3fe09b30820e62daa6c34f31b9e0c4ff34250d7
Jason Berry <jason.berry@algolia.com>
2024-08-12 20:51:48 +1000

    chore: use shipjs to update version in requirejs docs

--- ship.config.js
+++ ship.config.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = {
   shouldPrepare: ({ releaseType, commitNumbersPerType }) => {
     const { fix = 0 } = commitNumbersPerType;
@@ -7,5 +10,15 @@ module.exports = {
     return true;
   },
   buildCommand: () => "yarn build && /bin/bash ./pre-deploy.sh",
+  versionUpdated: ({ version, dir }) => {
+    // update version in `docs/requirejs.md`
+    const requirejsDocsPath = path.resolve(dir, "docs", "requirejs.md");
+    const requirejsDocs = fs.readFileSync(requirejsDocsPath).toString();
+    const updatedRequirejsDocs = requirejsDocs.replace(
+      /(\/search-insights@)([^\/]+)(\/dist)/,
+      `$1${version}$3`
+    );
+    fs.writeFileSync(requirejsDocsPath, updatedRequirejsDocs);
+  },
   pullRequestTeamReviewers: ["events-platform"]
 };
