ebb41f8a365bdd6bc0339f8585189168c6213c3c
Jason Berry <jason.berry@algolia.com>
2024-08-23 14:52:39 +1000

    feat: update getFunctionalInterface to return the method being passed instead of just executing (#552)
    Fixes #524

--- lib/__tests__/_getFunctionalInterface.test.ts
+++ lib/__tests__/_getFunctionalInterface.test.ts
@@ -2,7 +2,11 @@
 
 import { getFunctionalInterface } from "../_getFunctionalInterface";
 import AlgoliaAnalytics from "../insights";
-import type { InsightsClient } from "../types";
+import type { InsightsClient, InsightsMethodMap } from "../types";
+
+jest.mock("../../package.json", () => ({
+  version: "1.0.1"
+}));
 
 describe("_getFunctionalInterface", () => {
   let aa: InsightsClient;
@@ -22,5 +26,63 @@ describe("_getFunctionalInterface", () => {
     expect(console.warn).toHaveBeenCalledWith(
       "The method `unknown-function` doesn't exist."
     );
+  });
+
+  it.each<keyof InsightsMethodMap>([
+    "addedToCartObjectIDs",
+    "addedToCartObjectIDsAfterSearch",
+    "clickedFilters",
+    "clickedObjectIDs",
+    "clickedObjectIDsAfterSearch",
+    "convertedFilters",
+    "convertedObjectIDs",
+    "convertedObjectIDsAfterSearch",
+    "purchasedObjectIDs",
+    "purchasedObjectIDsAfterSearch",
+    "viewedFilters",
+    "viewedObjectIDs"
+  ])("returns a promise to be waited on for %s call", async (method) => {
+    aa("init", {
+      appId: "appId",
+      apiKey: "apiKey"
+    });
+    const result = await aa(method, {
+      eventName: "click",
+      objectIDs: ["1", "2"],
+      index: "index"
+    });
+    expect(result).toBe(true);
+  });
+
+  it("returns a promise to be waited on for sendEvents call", async () => {
+    aa("init", {
+      appId: "appId",
+      apiKey: "apiKey"
+    });
+    const result = await aa("sendEvents", [
+      {
+        eventName: "click",
+        eventType: "click",
+        objectIDs: ["1", "2"],
+        index: "index"
+      }
+    ]);
+    expect(result).toBe(true);
+  });
+
+  it("returns values on sync calls", () => {
+    aa("init", {
+      appId: "appId",
+      apiKey: "apiKey",
+      userToken: "userToken",
+      authenticatedUserToken: "authenticatedUserToken"
+    });
+    expect(aa("getAuthenticatedUserToken")).toBe("authenticatedUserToken");
+    expect(aa("getUserToken")).toBe("userToken");
+    expect(aa("getVersion")).toBe("1.0.1");
+    expect(aa("setAuthenticatedUserToken", "newAuthenticatedUserToken")).toBe(
+      "newAuthenticatedUserToken"
+    );
+    expect(aa("setUserToken", "newUserToken")).toBe("newUserToken");
   });
 });
